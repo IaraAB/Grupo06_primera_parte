908c74ce8f0a251c58cedf002f9c2d5c037100a3
IaraAB <105770972+IaraAB@users.noreply.github.com>
2023-05-01 01:32:07 -0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package Presentacion;
+
+public class Main {
+	
+	private static Sistema sistema=Sistema.getInstancia();
+
+	public static void main(String[] args) {
+		
+		
+		sistema.main();
+	}
+
+}
